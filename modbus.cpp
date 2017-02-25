c3a6fe67eee27b24e34835ce98227852e72f9203
Majkel44 <m.werner9205@gmail.com>
2017-02-25 21:47:41 +0100

    - added class for handling modbus - added onReadReady signal - added onFinishedWriteRequest signal - added onReadError signal

---
+++ modbus.cpp
@@ -0,0 +1,175 @@
+#include "modbus.h"
+
+Modbus::Modbus(){
+
+    modbusSlave = new QModbusRtuSerialMaster(this);
+
+
+}
+
+
+bool Modbus::connectToSlave(QString port, int baud){
+
+
+    if (!modbusSlave) return false;
+
+    if (modbusSlave->state() != QModbusDevice::ConnectedState){
+
+        modbusSlave->setConnectionParameter(QModbusDevice::SerialPortNameParameter,port);
+        modbusSlave->setConnectionParameter(QModbusDevice::SerialParityParameter,QSerialPort::NoParity);
+        modbusSlave->setConnectionParameter(QModbusDevice::SerialBaudRateParameter,baud);
+        modbusSlave->setConnectionParameter(QModbusDevice::SerialDataBitsParameter,QSerialPort::Data8);
+        modbusSlave->setConnectionParameter(QModbusDevice::SerialStopBitsParameter,QSerialPort::OneStop);
+        modbusSlave->setTimeout(10000);
+        modbusSlave->setNumberOfRetries(3);
+
+    }
+
+    if (!modbusSlave->connectDevice()){
+
+        return false;
+    }else{
+
+        return true;
+
+    }
+
+
+}
+
+
+
+QModbusDataUnit Modbus::prepareRequest(int regAdress, int regType){
+
+    return QModbusDataUnit( (QModbusDataUnit::RegisterType)regType, regAdress,10);
+
+}
+
+void Modbus::executeReadRequest(int slaveAdress, int regAdress, int regType){
+
+    if (!modbusSlave) return;
+
+    if (auto *reply = modbusSlave->sendReadRequest(prepareRequest(regAdress,regType),slaveAdress)){
+
+        if (!reply->isFinished())
+
+
+            connect(reply,&QModbusReply::finished,this,&Modbus::readReady);
+
+
+            else
+
+            delete reply;
+
+
+
+    }else{
+
+        qDebug () << "Error: " << modbusSlave->errorString();
+
+    }
+
+}
+
+QModbusDataUnit Modbus::prepareWriteRequest(int regAdress, int regType){
+
+    return QModbusDataUnit((QModbusDataUnit::RegisterType)regType,regAdress,1);
+
+}
+
+void Modbus::executeWriteRequest(int slaveAdress, int regAdress, int regType, int value){
+
+    QModbusDataUnit writeRequest = prepareWriteRequest(regAdress,regType);
+
+    for (uint i=0; i<writeRequest.valueCount(); i++){
+
+        writeRequest.setValue(i,value);
+    }
+
+    if (auto * reply = modbusSlave->sendWriteRequest(writeRequest,slaveAdress)){
+
+        if (!reply->isFinished()){
+
+            //TODO: conncet signal finished to slot
+            connect(reply,&QModbusReply::finished,this,&Modbus::writeRequestFinished);
+
+        }else{
+
+            reply->deleteLater();
+        }
+    }else{
+
+        qDebug () << "Error: " << reply->errorString();
+    }
+
+
+}
+
+
+void Modbus::readReady(){
+
+    auto reply = qobject_cast<QModbusReply*>(sender());
+
+    if (!reply) return;
+
+    if (reply->error()==QModbusDevice::NoError){
+
+        const QModbusDataUnit modbusDataUnit = reply->result();
+        emit onReadReady(modbusDataUnit);
+
+        for (uint i=0; i<modbusDataUnit.valueCount();i++){
+
+            qDebug () << "Value: " << modbusDataUnit.value(i);
+
+        }
+
+    }
+
+    else if (reply->error()==QModbusDevice::ProtocolError){
+
+        qDebug () << "Protocol error: " << reply->errorString();
+        emit onReadError(reply->errorString());
+
+    }else {
+
+        qDebug () << "Error: " << reply->errorString();
+        emit onReadError(reply->errorString());
+
+    }
+
+
+}
+
+void Modbus::writeRequestFinished(){
+
+    auto reply  = qobject_cast<QModbusReply*>(sender());
+
+    if (!reply) return;
+
+    if (reply->error()==QModbusDevice::ProtocolError){
+
+        qDebug () << "Protocol error: " << reply->errorString();
+        emit onFinishedWriteRequest("Protocol error: "+reply->errorString());
+
+    }else if (!reply->error()==QModbusDevice::NoError){
+
+        qDebug () << "Other error: " << reply->errorString();
+         emit onFinishedWriteRequest("Other error: "+reply->errorString());
+    }
+    else{
+
+        qDebug () << "Send request status: ok ";
+         emit onFinishedWriteRequest("OK!");
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
---
+++ modbus.h
@@ -0,0 +1,58 @@
+#ifndef MODBUS_H
+#define MODBUS_H
+
+#include <QObject>
+#include <QModbusDataUnit>
+#include <QModbusRtuSerialMaster>
+#include <QSerialPort>
+#include <QDebug>
+
+class QModbusReply;
+class QModbusClient;
+
+
+class Modbus : public QObject
+{
+
+    Q_OBJECT
+
+public:
+    Modbus();
+    bool connectToSlave(QString port, int baud);
+    void executeReadRequest(int slaveAdress, int regAdress, int regType); //passing adress of slave, adress of register and register type
+    void executeWriteRequest(int slaveAdress, int regAdress, int regType, int value); //passing adress of slave, adress of register and register type
+
+    enum RegType { DiscreteInputs = 1, Coils = 2, InputRegisters = 3, HoldingRegisters = 4 };
+
+
+private:
+    QModbusReply *modbusReply;
+    QModbusClient *modbusSlave;
+
+    QModbusDataUnit prepareRequest(int regAdress, int regType);
+    QModbusDataUnit prepareWriteRequest(int regAdress, int regType);
+
+
+private slots:
+    void readReady();
+    void writeRequestFinished();
+
+signals:
+
+    void onFinishedWriteRequest(const QString & msg);
+    void onReadError(const QString & msg);
+    void onReadReady(const QModbusDataUnit & reg);
+
+
+
+
+
+
+
+
+
+
+
+};
+
+#endif // MODBUS_H
